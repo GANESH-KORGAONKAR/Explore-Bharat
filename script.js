6d0b94745e6193faed01b5706c93d9f748cb11a4
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2025-07-17 12:20:05 +0530

    updated code with responsive css

--- script.js
+++ script.js
@@ -3,25 +3,30 @@ gsap.to(".loc-img img", {
   width : "100%",
   duration : 5,
   stagger : 5,
+  opacity: 1
 })
 
 gsap.to(".loc-name h1", {
   ease : Expo.easeInOut,
   stagger : 5,
-  top: 0
+  top: 0,
+  opacity: 1
+
 })
 
 gsap.to(".loc-name h1", {
   delay : 5,
   ease : Expo.easeInOut,
   stagger : 5,
-  top: "100%"
+  top: "100%",
 })
 
 gsap.to(".loc-info p", {
   ease : Expo.easeInOut,
   stagger : 5,
-  top: 0
+  top: 0,
+  opacity: 1
+
 })
 
 gsap.to(".loc-info p", {
